00bfe5eac7922c9fc4bb2522c641850bad0940a3
Christopher D. Long <galizur@gmail.com>
2015-09-25 20:37:48 -0400

    NCAA division rankings.

--- ncaa_pbp/sos/division_ranking.sql
+++ ncaa_pbp/sos/division_ranking.sql
@@ -4,40 +4,40 @@ create temporary table r (
        school_id	 integer,
        div	 	 integer,
        year	 	 integer,
---       str	 	 float
+       str	 	 float,
        ofs	 	 float,
-       dfs	 	 float
---       sos	 	 float
+       dfs	 	 float,
+       sos	 	 float
 );
 
 insert into r
-(school_id,div,year,ofs,dfs)
+(school_id,div,year,str,ofs,dfs,sos)
 (
 select
-t.school_id,
-t.div_id as div,
+t.team_id,
+t.division as div,
 sf.year,
---(sf.strength*o.exp_factor/d.exp_factor)::numeric(9,3) as str,
+(sf.strength*o.exp_factor/d.exp_factor)::numeric(9,3) as str,
 (offensive*o.exp_factor)::numeric(9,3) as ofs,
-(defensive*d.exp_factor)::numeric(9,3) as dfs
---schedule_strength::numeric(9,3) as sos
+(defensive*d.exp_factor)::numeric(9,3) as dfs,
+schedule_strength::numeric(9,3) as sos
 from ncaa_pbp._schedule_factors sf
-left outer join ncaa_pbp.schools_divisions t
-  on (t.school_id,t.year)=(sf.school_id,sf.year)
+left outer join ncaa_pbp.teams t
+  on (t.team_id,t.year)=(sf.school_id,sf.year)
 left outer join ncaa_pbp._factors o
-  on (o.parameter,o.level)=('o_div',length(t.division)::text)
+  on (o.parameter,o.level)=('o_div',t.division::text)
 left outer join ncaa_pbp._factors d
-  on (d.parameter,d.level)=('d_div',length(t.division)::text)
+  on (d.parameter,d.level)=('d_div',t.division::text)
 where sf.year in (2016)
-and t.school_id is not null
+and t.team_id is not null
 order by ofs desc);
 
 select
 year,
---exp(avg(ln(str)))::numeric(9,3) as str,
+exp(avg(ln(str)))::numeric(9,3) as str,
 exp(avg(ln(ofs)))::numeric(9,3) as ofs,
 exp(-avg(ln(dfs)))::numeric(9,3) as dfs,
---exp(avg(ln(sos)))::numeric(9,3) as sos,
+exp(avg(ln(sos)))::numeric(9,3) as sos,
 count(*) as n
 from r
 group by year
@@ -46,10 +46,10 @@ order by year asc;
 select
 year,
 div,
---exp(avg(ln(str)))::numeric(9,3) as str,
+exp(avg(ln(str)))::numeric(9,3) as str,
 exp(avg(ln(ofs)))::numeric(9,3) as ofs,
 exp(-avg(ln(dfs)))::numeric(9,3) as dfs,
---exp(avg(ln(sos)))::numeric(9,3) as sos,
+exp(avg(ln(sos)))::numeric(9,3) as sos,
 count(*) as n
 from r
 where div is not null
